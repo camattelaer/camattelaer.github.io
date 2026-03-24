ef070e96b061695a9deeaf5a6ece559ac6639287
camattelaer <60655504+camattelaer@users.noreply.github.com>
2026-03-24 19:04:31 +0100

    local hosting website

---
+++ _site/README.md
@@ -0,0 +1,3 @@
+# camattelaer.github.io
+
+test
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "My First Research Post",
+        "excerpt":"test entry  ","categories": [],
+        "tags": [],
+        "url": "/computational-chemistry/test/",
+        "teaser": null
+      },{
+        "title": "It's alive!",
+        "excerpt":"At the end of August in 2025, v1.0 of my home cluster was fully operational. It was a moment I am still very proud of because I dreamed of having my own little cluster at home ever since venturing in the world of computational chemistry. My home cluster. I built...","categories": [],
+        "tags": [],
+        "url": "/sysadmin/2026-03-07/",
+        "teaser": "/assets/images/sysadmin/20250826_121202.jpg"
+      },{
+        "title": "Compiling AMBER for modern GPU architecture",
+        "excerpt":"The dread of every computational chemist is compiling your scientific software. If you rely on campus or government clusters, you can probably rely on real sysadmins. However, when you are running your own system, you have to roll up your sleeves and do the dirty work yourself. The compilation of...","categories": [],
+        "tags": [],
+        "url": "/sysadmin/2026-03-11/",
+        "teaser": null
+      },{
+        "title": "Running your Github Pages website locally",
+        "excerpt":"My Github contributions were temporarily and artificially boosted when I started my website, see Figure 1. I really did not want my Github contribution overview to be biased because of my inability to build this website. For this particular reason, I decided to figure out how to publish the site...","categories": [],
+        "tags": [],
+        "url": "/sysadmin/2026-03-24/",
+        "teaser": "/assets/images/sysadmin/2026-03-24/website_local.jpg"
+      }]
